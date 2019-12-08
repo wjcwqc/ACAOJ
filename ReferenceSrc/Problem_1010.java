6eacf137c62b0c89d96f953e1a011adae1829a99
MeiWeiDing <1635170329@qq.com>
2019-12-08 17:41:13 +0800

    修改1010,1011

--- ReferenceSrc/Problem_1010.java
+++ ReferenceSrc/Problem_1010.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-import static java.lang.Math.pow;
-
-
 public class Problem_1010 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
-        int N = cin.nextInt();
-        String s = String.valueOf(N);
+        String s = cin.next();
+        int N = Integer.parseInt(s);
         if (N >= 1000 || N < 100) return;
-        int a = s.charAt(0), b = s.charAt(1), c = s.charAt(2);
-        int x1 = (int) pow(a, 3), x2 = (int) pow(b, 3), x3 = (int) pow(c, 3);
+        int a = Integer.parseInt(s.charAt(0)+"");
+        int b = Integer.parseInt(s.charAt(1)+"");
+        int c = Integer.parseInt(s.charAt(2)+"");
+        int x1 = a*a*a, x2 = b*b*b, x3 = c*c*c;
         if (N == x1 + x2 + x3) System.out.println(1);
         else System.out.println(0);
         cin.close();
