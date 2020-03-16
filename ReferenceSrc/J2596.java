d291e6d677a315ef9d544111826f94a118266180
wjcwqc <30833114+wjcwqc@users.noreply.github.com>
2020-03-16 18:06:53 +0800

    这题是白嫖的老梅的 Signed-off-by: wjcwqc <wjcwqc@gmail,com>
    Signed-off-by: wjcwqc <wjcwqc@gmail,com>

---
+++ ReferenceSrc/J2596.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020. wjcwqc.All rights reserved.
+ * License:GNU Public License
+ * Project name:ACAOJ
+ * Auther:wjcwqc
+ * Date:2020/3/16 下午5:28
+ */
+
+import java.util.*;
+
+public class J2596 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        int k = cin.nextInt();
+        int count = 0;
+        boolean[] light = new boolean[n];
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((j + 1) % i == 0) light[j] = !light[j];
+            }
+        }
+        for (int i = 0; i < light.length; i++) {
+            if (light[i]) {
+                System.out.print((i + 1) + " ");
+                count++;
+            }
+        }
+        if (count == 0) System.out.println("None");
+        cin.close();
+    }
+}
