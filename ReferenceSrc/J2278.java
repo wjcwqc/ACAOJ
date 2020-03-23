ae9e9d39369a21f9ac50d529d9a0228f6b332613
MeiWeiDing <1635170329@qq.com>
2020-03-23 18:37:56 +0800

    fixed J2278

--- ReferenceSrc/J2278.java
+++ ReferenceSrc/J2278.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.*;
 
 public class J2278 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
-        int N = cin.nextInt();
-        System.out.println(Fibonacci(N));
+        int n = cin.nextInt();
+        System.out.println(Fibonacci(n));
         cin.close();
     }
 
-    private static int Fibonacci(int n) {
-        if (n == 1 || n == 2)
-            return 1;
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    private static BigInteger Fibonacci(int n) {
+        BigInteger a1=new BigInteger("1");
+        BigInteger a2=new BigInteger("2");
+        BigInteger a3=new BigInteger(n+"");
+        for(int i=3;i<=n;i++) {
+            a3 = a1.add(a2);
+            a1=a2;
+            a2=a3;
+        }
+        return a3;
     }
 }
