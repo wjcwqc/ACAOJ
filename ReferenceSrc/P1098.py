1c95760931b20406b11a9b7095aceb22657ca137
囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>
2020-03-29 20:06:06 +0800

    Update P1098.py

--- ReferenceSrc/P1098.py
+++ ReferenceSrc/P1098.py
@@ -2,7 +2,7 @@ def pailie(head="",string=""):
     global result
     if len(string)>1:
         for num,father_string in enumerate(string):
-            if father_string in string[0:num]:#如果字符与前面的重复说明拍过顺序了
+            if father_string in string[0:num]:#如果字符与前面的重复说明排过顺序了
                 continue
             pailie(head+father_string,string.replace(father_string,"",1))#只能替换一次
     else:
