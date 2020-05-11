0eb1fc209dd3387870d7ca763b67d1e17e9d6085
MeiWeiDing <1635170329@qq.com>
2020-05-11 17:31:40 +0800

    addJ1117

---
+++ ReferenceSrc/J1117.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Copyright (c) 2020. wjcwqc.All rights reserved.
+ * License:GNU Public License
+ * Project name:ACAOJ
+ * Auther:wjcwqc
+ * Date:2020/5/11 下午4:58
+ */
+public class J1117 {
+    static Scanner cin = new Scanner(System.in);
+
+    static public void main(String[] args) {
+        while (cin.hasNext()) {
+            int a = 100 * cin.nextInt();
+            int b = cin.nextInt();
+            if (a == 0 && b == 0) break;
+            for (int i = 0; i < 100; i++) {
+                if ((a + i) % b == 0) {
+                    if (i < 10) System.out.printf("0");
+                    System.out.printf("%d ", i);
+                }
+            }
+            System.out.printf("\n");
+        }
+
+    }
+}
+
