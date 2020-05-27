cb429f173da809213c436d878806955a2e9653c1
wjcwqc <wjcwqc@gmail,com>
2020-05-27 16:12:31 +0800

    Revert "none"
    This reverts commit 4ec67a389ad6d1e72c35a5a5ad06b4304c56b66e.

--- ReferenceSrc/J1744dr.java
+++ ReferenceSrc/J1744dr.java
@@ -1,2 +1,13 @@
+/*
+ * Copyright (c) 2020. wjcwqc.All rights reserved.
+ * License:GNU Public License
+ * Project name:ACAOJ
+ * Auther:wjcwqc
+ * Date:2020/5/25 下午8:35
+ */
+
 public class J1744dr {
+    public static void main(String[] args) {
+        System.out.println("192384576\n219438657\n273546819\n327654981");
+    }
 }
